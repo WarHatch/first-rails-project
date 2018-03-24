0716bf8806a6762983e981c9b5747602222cc42a
WarHatch <staskeviciuskarolis@gmail.com>
2018-03-24 18:29:49 +0200

    Tags are deletable

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,5 +5,14 @@ class TagsController < ApplicationController
 
     def show
         @tag = Tag.find(params[:id])
-    end      
+    end 
+    
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+
+        flash.notice = "Tag '#{@tag.name}' deleted."
+
+        redirect_to tags_path
+    end
 end
--- db/migrate/20180324181951_create_taggings.rb
+++ db/migrate/20180324181951_create_taggings.rb
@@ -1,8 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration[5.1]
   def change
     create_table :taggings do |t|
-      t.references :tag, foreign_key: true
-      t.references :article, foreign_key: true
+      t.references :tag, foreign_key: {on_delete: :cascade}
+      t.references :article, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
